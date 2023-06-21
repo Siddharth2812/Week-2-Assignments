aea9ad8a9fdd061f555f9ed7ce87b00b472ca7c1
SiddharthReddy <siddharthreddy@meet.example.org>
2023-06-21 18:09:55 +0530

    Auth server Complete

--- 02-nodejs/authenticationServer.js
+++ 02-nodejs/authenticationServer.js
@@ -1,37 +1,138 @@
-/**
-  You need to create a HTTP server in Node.js which will handle the logic of an authentication server.
-  - Don't need to use any database to store the data.
+const http = require('http');
+const server = require('../authenticationServer');
 
-  - Save the users and their signup/login data in an array in a variable
-  - You can store the passwords in plain text (as is) in the variable for now
+const email = 'testuser@gmail.com';
+const password = 'testpassword';
+const firstName = "kirat"
+const lastName = "kirat"
 
-  The expected API endpoints are defined below,
-  1. POST /signup - User Signup
-    Description: Allows users to create an account. This should be stored in an array on the server, and a unique id should be generated for every new user that is added.
-    Request Body: JSON object with username, password, firstName and lastName fields.
-    Response: 201 Created if successful, or 400 Bad Request if the username already exists.
-    Example: POST http://localhost:3000/signup
 
-  2. POST /login - User Login
-    Description: Gets user back their details like firstname, lastname and id
-    Request Body: JSON object with username and password fields.
-    Response: 200 OK with an authentication token in JSON format if successful, or 401 Unauthorized if the credentials are invalid.
-    Example: POST http://localhost:3000/login
+describe('API Tests', () => {
+  let globalServer;
+  beforeAll((done) => {
+      if (globalServer) {
+        globalServer.close();
+      }
+    globalServer = server.listen(3000);
+    done()
+  });
 
-  3. GET /data - Fetch all user's names and ids from the server (Protected route)
-    Description: Gets details of all users like firstname, lastname and id in an array format. Returned object should have a key called users which contains the list of all users with their email/firstname/lastname.
-    The users username and password should be fetched from the headers and checked before the array is returned
-    Response: 200 OK with the protected data in JSON format if the username and password in headers are valid, or 401 Unauthorized if the username and password are missing or invalid.
-    Example: GET http://localhost:3000/data
+  afterAll((done) => {
+    globalServer.close(done);
+  });
 
-  - For any other route not defined in the server return 404
+  it('should allow a user to sign up', async () => {
+    const requestBody = JSON.stringify({ email, password, firstName, lastName });
 
-  Testing the server - run `npm run test-authenticationServer` command in terminal
- */
+    const options = {
+      method: 'POST',
+      path: '/signup',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': requestBody.length,
+      },
+    };
 
-const express = require("express")
-const PORT = 3000;
-const app = express();
-// write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
+    const response = await sendRequest(options, requestBody);
 
-module.exports = app;
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toBe('Signup successful');
+  });
+
+  it('should allow a user to login', async () => {
+    const requestBody = JSON.stringify({ email, password });
+
+    const options = {
+      method: 'POST',
+      path: '/login',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': requestBody.length,
+      },
+    };
+
+    const response = await sendRequest(options, requestBody);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBeDefined();
+
+    const responseBody = JSON.parse(response.body);
+    expect(responseBody.email).toBe(email);
+    expect(responseBody.firstName).toBe(firstName);
+    expect(responseBody.lastName).toBe(lastName);
+
+    authToken = responseBody.authToken;
+  });
+
+  it('should return unauthorized for accessing protected data without authentication', async () => {
+    const options = {
+      method: 'GET',
+      path: '/data',
+      headers: {
+        email,
+        password: "",
+      },
+    };
+
+    const response = await sendRequest(options);
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toBe('Unauthorized');
+  });
+
+  it('should return the users for accessing protected data with authentication', async () => {
+    const options = {
+      method: 'GET',
+      path: '/data',
+      headers: {
+        email,
+        password
+      },
+    };
+
+    const response = await sendRequest(options);
+
+    expect(response.statusCode).toBe(200);
+
+    const responseBody = JSON.parse(response.body);
+    console.log(responseBody);
+    expect(responseBody.users.length).toBe(1);
+  });
+});
+
+function sendRequest(options, requestBody) {
+  return new Promise((resolve, reject) => {
+    const req = http.request(
+      {
+        ...options,
+        host: 'localhost',
+        port: 3000,
+      },
+      (res) => {
+        let body = '';
+
+        res.on('data', (chunk) => {
+          body += chunk;
+        });
+
+        res.on('end', () => {
+          resolve({
+            statusCode: res.statusCode,
+            headers: res.headers,
+            body,
+          });
+        });
+      }
+    );
+
+    req.on('error', (err) => {
+      reject(err);
+    });
+
+    if (requestBody) {
+      req.write(requestBody);
+    }
+
+    req.end();
+  });
+}
